93366ad878ec86104a0b87316c07b2e2a4f56df7
Madusha dilshan <143088487+madushabangamuwage2001@users.noreply.github.com>
2025-02-12 12:11:55 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const router = require("./ToDoRoute");
+
+
+require("dotenv").config();
+
+console.log("MongoDB URI:", process.env.MONGODB_URL); // Debugging line
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+app.use(router);
+
+app.listen(PORT, () => console.log(`listening on: ${PORT}`));
