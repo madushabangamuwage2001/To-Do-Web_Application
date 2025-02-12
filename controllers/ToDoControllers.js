00007f74177fcd00b5c03aeccaf9a3bf912a5bcb
Madusha dilshan <143088487+madushabangamuwage2001@users.noreply.github.com>
2025-02-13 00:21:12 +0530

    create fonend

--- controllers/ToDoControllers.js
+++ controllers/ToDoControllers.js
@@ -1,9 +1,11 @@
 // controllers/ToDoControllers.js
 
+const ToDoModel = require("../models/ToDoModel"); // Ensure correct path and name
+
 
 module.exports.getToDO = async (req, res) => {
   try {
-    const todos = await ToDoModel.find();  // Fixed variable name from 'toDo' to 'todos'
+    const todos = await ToDoModel.find();
     res.status(200).json(todos);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -19,4 +21,20 @@ module.exports.saveToDO = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+};
+
+module.exports.updateToDO = async (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel
+    .findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Updated successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDO = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel
+    .findByIdAndDelete(_id)
+    .then(() => res.send("Deleted successfully"))
+    .catch((err) => console.log(err));
 };
--- routes/ToDoRoute.js
+++ routes/ToDoRoute.js
@@ -1,9 +1,11 @@
 // routes/ToDoRoute.js
 const express = require("express");
-const { getToDO, saveToDO } = require("../controllers/ToDoControllers");
+const { getToDO, saveToDO, updateToDO, deleteToDO } = require("../controllers/ToDoControllers");
 const router = express.Router();
 
 router.get("/", getToDO);
-router.post("/save", saveToDO);  // Fixed typo in 'router'
+router.post("/save", saveToDO);  
+router.post("/update", updateToDO);
+router.post("/delete", deleteToDO);
 
 module.exports = router;
