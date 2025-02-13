1a9db32b24b50a6c15da551a899382a080c1b800
Madusha dilshan <143088487+madushabangamuwage2001@users.noreply.github.com>
2025-02-14 01:21:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require("cors")
 
-const routes = require('./routes/ToDoRoutes')
+const routes = require('./routes/ToDoRoute')
 
 require('dotenv').config()
 
