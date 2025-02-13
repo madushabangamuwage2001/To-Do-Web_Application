dd205a613120743b5d56c390ff4bdae529bb8a35
Madusha dilshan <143088487+madushabangamuwage2001@users.noreply.github.com>
2025-02-13 23:10:59 +0530

    get fontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo"; 
+import { getAllToDo } from "./utils/HandleApi";
+
 function App() {
+  const [toDo, setTodo] = useState([]);
+
+  useEffect(() => {
+
+    getAllToDo(setTodo);
+  }, []);
+
   return (
     <div className="App">
       <div className="Container">
         <h1>To-Do List</h1>
         <div className="top">
-          <input type="text" placeholder="ADD TODO..."></input>
+          <input type="text" placeholder="ADD TODO..." />
 
           <div className="add">ADD</div>
-
-
-
-
-
-
+        </div>
+        <div className="list">
+          {toDo.map((item) => (
+            <ToDo key={item._id} text={item.text} />
+          ))}
+          <ToDo text="Hello" />
+          <ToDo text="Hello" />
+          <ToDo text="Hello" />
         </div>
       </div>
     </div>
